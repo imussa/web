ebc5983717c9fd9294ed559be12a5810ac3fbe68
imussa <shazhiwei96@163.com>
2020-06-11 22:50:51 +0800

    完成GitHub授权,获取用户token

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,35 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+      <v-menu v-if="token"
+        offset-y
+      >
+        <template v-slot:activator="{ on, attrs}">
+          <v-btn
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <span class="mr-2">用户名</span>
+            <v-icon>mdi-open-in-new</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item @click="logout">
+            <v-list-item-title>退出</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-btn v-else
+        @click="login"
+        outlined
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span>登录</span>
       </v-btn>
     </v-app-bar>
 
@@ -55,6 +55,26 @@ export default {
 
   data: () => ({
     //
+    gituhbUrl: "https://github.com/login/oauth/authorize?client_id=43fdd91adee9e67a78ad&redirect_uri=https%3A%2F%2Fimussatools.herokuapp.com%2Foauth%2Fgithub_callback%2F&scope=user%3Aemail",
+    // gituhbUrl: "http://localhost:8000/oauth/github_login/",
+    token: ''
   }),
+
+  methods: {
+    login () {
+      let vm = this
+      window.open(vm.gituhbUrl, 'newwindow', 'height=500, width=500, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=n o, status=no')
+      // 通过监听，父页面可以拿到子页面传递的token，父(前端页面)，子(小窗)
+      window.addEventListener('message', function (e) {
+        vm.setToken(e.data)
+      },false)
+    },
+    logout () {
+      this.token = ''
+    },
+    setToken (token) {
+      this.token = token
+    }
+  }
 };
 </script>
